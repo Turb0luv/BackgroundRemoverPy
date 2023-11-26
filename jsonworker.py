a79dbe59b0a297f6727bf1958720ff3b25976681
Dima <you@example.com>
2023-11-26 23:04:07 +0300

    Added JSONWorker Updated README.md

---
+++ jsonworker.py
@@ -0,0 +1,97 @@
+import json
+import os
+import re
+
+################################CONFIG##########################################
+# Создание ссылок на файлы
+check_path = r"Фулл"  # Папка с файлами(изо)
+sample = r"https://xn--80acmfijjefoglo2a.xn--90ais/assets/"  # Сэмпл ссылка
+dot = r'.webp'  # Расширение файла
+##################################################################################
+json_file = r"products-1700949482.json"  # Исходный файл
+exit_json = r'products-ready.json'  # Готовый файл/Исходный для создания незаконченными изо csv
+
+color_file = r"colors.json"  # Файл с цветами
+exit_csv = r'unfilled.csv'  # Готовый файл с незаконченными изо csv
+
+
+def url_id_maker():
+    files = os.listdir(check_path)
+    urls = []
+    mk = []
+    ids = []
+    for file in files:
+        urls.append(f'{sample}{file}')
+        mk.append(file[:5])
+    [ids.append(x) for x in mk if x not in ids]
+    return ids, urls
+
+
+def make_dct():
+    dct = {}
+    ids, urls = url_id_maker()
+    for id in ids:
+        mt = re.findall(r'{0}\d'.format(f'{sample}{id}_'), str(urls))
+        for i in range(len(mt)):
+            mt[i] = f'{mt[i]}{dot}'
+        kv = {id: mt}
+        dct.update(kv)
+    return dct
+
+
+def make_json():
+    with open(json_file, 'r', encoding="utf-8") as f:
+        data = json.load(f)
+    fold = make_dct()
+    for fid, urls in fold.items():
+        for i in range(len(data)):
+            if data[i]['id'] == int(fid):
+                # print(f'{fid}')
+                data[i]['imgUrls'] = urls
+    with open(exit_json, 'x', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False)
+    print('Обработанный JSON готов')
+
+
+def make_csv():
+    with open(exit_json, 'r', encoding="utf-8") as f:
+        data = json.load(f)
+    with open(color_file, 'r', encoding="utf-8") as f:
+        clrs = json.load(f)
+    colors = {}
+    for i in clrs['data']:
+        cl = {i: clrs["data"][i]["name"]}
+        colors.update(cl)
+    lst = []
+    for i in range(len(data)):
+        if not data[i]['imgUrls']:
+            idx = data[i]['id']
+            brand = data[i]['brand']
+            name = data[i]['name']
+            description = data[i]['description']
+            color = colors.get(f'{i}')
+            lst.append(f'{idx},"{brand}","{name}","{description}",{color}')
+    with open(exit_csv, 'x', encoding="utf-8") as f:
+        for i in lst:
+            f.write(f'{str(i)}\n')
+    print('CSV ооздан')
+
+
+def main():
+    print('Действие')
+    print('1. Обработка JSON')
+    print('2. Создание csv из JSON(пустые urlID)')
+    print('3. Всё сразу(JSON, csv)')
+    print('!!!Убедитесь в правильности указанных данных в конфиге!!!')
+    sw = int(input())
+    if sw == 1:
+        make_json()
+    elif sw == 2:
+        make_csv()
+    elif sw == 3:
+        make_json()
+        make_csv()
+
+
+if __name__ == "__main__":
+    main()
